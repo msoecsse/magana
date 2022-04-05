34b381566db0c109b49c57eb9abe77c450e763de
Dr. Jonathon Magana <jonathoncmagana@gmail.com>
2022-04-05 12:32:21 -0500

    Recursion example 2

--- src/week5/Notes5.java
+++ src/week5/Notes5.java
@@ -1,5 +1,7 @@
 package week5;
 
+import java.util.Optional;
+
 public class Notes5 {
     static int count = 0;
     public static void main(String[] args) {
@@ -7,7 +9,15 @@ public class Notes5 {
            // System.out.println(count++);
            // main(null);
       //  }
-        System.out.println(sumToN(3));
+     //   System.out.println(sumToN(3));
+       /* int[] Is = {1, 2, 3};
+        something("jsfkj",2, 3, 4, 5);
+        something("fdsf", Is);
+        Optional<String> myString = Optional.of("daw");
+        myString =Optional.empty();
+
+        */
+        System.out.println(fib(8));
     }
 
     // sum up all numbers including n.
@@ -15,5 +25,21 @@ public class Notes5 {
         if (n==1)
             return 1;
         return sumToN(n-1) + n;
+    }
+
+    public static void something (String name, int...arguments){
+        System.out.println(name);
+        System.out.println (arguments.length);
+    }
+
+    // fib(n) = fib(n-1) + fib(n-2)
+
+    public static int fib(int n){
+        // show through an exception for negative numbers
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        return fib(n-1) + fib(n-2);
     }
 }
